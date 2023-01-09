43500bf5032b5d2b9c9d7ec19d688a5e7628823f
CCL-Codes <81848055+CCL-Codes@users.noreply.github.com>
2023-01-09 16:19:37 +0600

    manifest.py
    can't installed

--- black_stone_customization/__manifest__.py
+++ black_stone_customization/__manifest__.py
@@ -14,14 +14,14 @@
     'category': 'base',
     'version': '0.1',
     'depends': ['account','stock','hr','sale','sale_stock','hr_recruitment'],
-    'data': [
-        'security/ir.model.access.csv',
-        'data/black_sequence.xml',
-        # 'security/account_security.xml',
-        'views/black_inventory_view.xml',
-        'views/black_sales_view.xml',
-        'views/black_account_view.xml',
-        'views/black_hr_view.xml',
-        # 'report/statement_report.xml'
+#     'data': [
+#         'security/ir.model.access.csv',
+#         'data/black_sequence.xml',
+#         # 'security/account_security.xml',
+#         'views/black_inventory_view.xml',
+#         'views/black_sales_view.xml',
+#         'views/black_account_view.xml',
+#         'views/black_hr_view.xml',
+#         # 'report/statement_report.xml'
     ],
 }
